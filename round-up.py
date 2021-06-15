729970a99b700a5ac5937307850f29aa6341bd56
Ben <benf97@gmail.com>
2021-06-15 20:31:21 +0100

    Update round-up.py
    class.

--- round-up.py
+++ round-up.py
@@ -4,19 +4,36 @@ import os
 
 # * API Credentials
 
-accessToken = os.environ.get('SANDBOX_ACCESS_TOKEN')
-accountUid = ""
-defaultCategory = ""
 
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {accessToken}"
-}
 
-r = requests.get(
-    'https://api-sandbox.starlingbank.com' + '/api/v2/accounts',
-    headers = headers
-).json()
+class roundUp:
 
-accountUid, defaultCategory = r['accounts'][0]['accountUid'], r['accounts'][0]['defaultCategory']
+    def __init__(self, weekStart):
+        self.weekStart = weekStart
+        self.accessToken = os.environ.get('SANDBOX_ACCESS_TOKEN') # * environment variable
+        self.accountUid = None
+        self.defaultCategory = None
+
+    # * api-sandbox GET Request
+    
+    def getreq(self, request_params):
+        return requests.get(
+            'https://api-sandbox.starlingbank.com' + request_params,
+            headers={
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.accessToken}"
+            }
+        ).json()
+    
+    # * SET sanbox customer credentials
+    
+    def setcreds(self, r):
+        self.accountUid = r['accounts'][0]['accountUid']
+        self.defaultCategory = r['accounts'][0]['defaultCategory']
+        print(r)
+        
+main = roundUp(None)
+accounts = main.getreq('/api/v2/accounts')
+main.setcreds(accounts)
+# feed = f"/api/v2/feed/account/{accountUid}/category/{defaultCategory}/transactions-between?minTransactionTimestamp=2021-06-01T12%3A34%3A56.000Z&maxTransactionTimestamp=2021-07-01T12%3A34%3A56.000Z"
