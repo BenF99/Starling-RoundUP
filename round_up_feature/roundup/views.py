b3131b90f0afcf95e6c87c077bdbcb6d2484372f
Ben <benf97@gmail.com>
2021-06-17 15:50:02 +0100

    Calls main function

--- round_up_feature/roundup/views.py
+++ round_up_feature/roundup/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import round_up
 # Create your views here.
 
 def home(request):
+    if(request.GET.get('mybtn')):
+        round_up.main()
     return render(request, 'main.html')
